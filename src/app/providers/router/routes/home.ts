27195d976c367f1d47f890b9b6f69159cee45347
andrey-dev-2001 <dev.andriy.zakharkiv@gmail.com>
2024-09-09 16:20:39 +0200

    andrey-dev: fix pagespeed

--- src/app/providers/router/routes/home.ts
+++ src/app/providers/router/routes/home.ts
@@ -1,12 +1,11 @@
 import type { RouteRecordRaw } from 'vue-router'
-import {HomePage} from "@/pages";
 
 export const routeName: string = 'home'
 
 export const route: RouteRecordRaw = {
     name: routeName,
     path: '/',
-    component: () => HomePage,
+    component: () => import('@/pages/home/ui/HomePage.vue'),
     meta: {
         title: 'Home - Shoppe',
         metaDescription: 'Shop the latest jewelry trends at Shoppe. From earrings to necklaces, discover our collection of stylish accessories.'
